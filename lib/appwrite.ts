5eb121c4c4e1409dd2b77862bd9769dfc5b6e0f9
winfriedweis <weis.winfried@icloud.com>
2025-12-19 17:09:45 +0100

    Adjustments on the Appwrite config

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,7 +1,19 @@
+import {Account, Client, Databases} from "react-native-appwrite";
+
 export const appwriteConfig = {
-    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
-    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!,
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!,
     platform: "com.winfriedweis.fooddeliveryapp",
     databaseId: "693fe308003869df3b88",
     tableId: "user"
 }
+
+export const client = new Client();
+
+client
+    .setEndpoint(appwriteConfig.endpoint)
+    .setProject(appwriteConfig.projectId)
+    .setPlatform(appwriteConfig.platform)
+
+export const account = new Account(client);
+export const database = new Databases(client);
